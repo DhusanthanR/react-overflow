9d589ef122c421ceff4e8f2777fae46241dfba23
DhusanthanR <dhusanthan1999g@gmail.com>
2020-05-15 22:08:06 +0530

    2 files modified

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Route,Redirect} from 'react-router-dom'
 import Login from './login/login';
 import Home from './home/home';
 export default class App extends Component{
--- app/home/bodyContent/question.js
+++ app/home/bodyContent/question.js
@@ -1,21 +1,23 @@
 import React,{Component} from 'react';
 
+
 export default class Question extends Component{
   constructor(){
     super()
     this.state={
       questions:localStorage.getItem("title"),
-      secondques:JSON.parse(localStorage.getItem("myobj"))
+      secondques:JSON.parse(localStorage.getItem("myobj")),
     }
   }
+  
   render(){
     console.log(this.state.secondques)
     return(<div>
     <div className="container">
     <h1>Questions.....?</h1>
     <div className="col-md-12">
-    <div className="row">
-    <h3>Title : {this.state.secondques.title}</h3><br/>
+    <div className="row d-flex flex-cloumn">
+    <h3>Title : {this.state.secondques.title}</h3>
     <h3>Question : {this.state.secondques.question}</h3>
     </div>
     </div>
